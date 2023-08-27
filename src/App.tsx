56beca3c9ef30a1763e858dbf09327d3814ab346
Thomas Farias <thomasfarias19@gmail.com>
2023-08-27 01:17:41 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyles />
+    </ThemeProvider>
+  )
+}
